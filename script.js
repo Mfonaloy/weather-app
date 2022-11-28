308452cf13ef9f032ef68a0ea93aeb79febb0b9f
Mfon Aloy <mfonobongsmn82@gmail.com>
2022-11-28 16:29:48 +0100

    Edited the html fie

--- script.js
+++ script.js
@@ -30,18 +30,26 @@ function formatDate(date) {
     cityElement.innerHTML = cityInput.value;
   }
   
-  function displayTemperature(response) {
+  function displayWeatherCondition(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
   
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
+
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
   }
+
   function searchCity(city) {
     let apiKey = "2ad3df86f01baa30d925420a827292df";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   
-    axios.get(apiUrl).then(displayTemperature);
+    axios.get(apiUrl).then(displayWeatherCondition);
   }
   
   function handleCity(e) {
@@ -50,20 +58,39 @@ function formatDate(date) {
     searchCity(city.value);
   }
   
-  function showPosition(position) {
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = `your latitude is ${position.coords.latitude} and your longitude is ${position.coords.longitude}`;
+  function searchLocation(position) {
+    let apiKey = "2ad3df86f01baa30d925420a827292df";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
+    position.coords.latitude
+  }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+ axios.get(apiUrl).then(displayWeatherCondition);
   }
-  function getCurrentPosition() {
-    navigator.geolocation.getCurrentPosition(showPosition);
+
+  function getCurrentLocation(event){
+    event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+  }
+  
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = 66;
+  }
+  
+  function convertToCelsius(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = 19;
   }
   let currentLocationButton = document.querySelector("#current-location-button");
-  currentLocationButton.addEventListener("click", getCurrentPosition);
+  currentLocationButton.addEventListener("click", getCurrentLocation);
   
   let dateElement = document.querySelector("#date");
   let currentTime = new Date();
-  dateElement.innerHTML = new Date(currentTime);
+  dateElement.innerHTML = formatDate(currentTime);
   
   let searchForm = document.querySelector("#search-form");
   searchForm.addEventListener("submit", handleCity);
   
+  searchCity("Abuja");
